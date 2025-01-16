c863efe05fcdf542d5f6d140818ce5dd0cd3d1a7
Debajyoti2004 <23je0291@iitism.ac.in>
2025-01-16 20:16:29 +0530

    Add files via upload

---
+++ model/AI_pipeline.py
@@ -0,0 +1,47 @@
+import os
+import pathway as pw
+from pathway.xpacks.llm.llms import OpenAIChat, prompt_chat_single_qa
+
+
+#  REST Connector config.
+HTTP_HOST = os.environ.get("PATHWAY_REST_CONNECTOR_HOST", "127.0.0.1")
+HTTP_PORT = os.environ.get("PATHWAY_REST_CONNECTOR_PORT", "8080")
+
+#  LLM model parameters
+#  For OPENAI API
+API_KEY = os.getenv('API_KEY')
+#  Specific model from OpenAI. You can also use gpt-3.5-turbo for faster responses.
+MODEL_LOCATOR = "gpt-4"
+# Controls the stochasticity of the openai model output.
+TEMPERATURE = 0.0
+# Max completion tokens
+MAX_TOKENS = 50
+
+
+class QueryInputSchema(pw.Schema):
+    query: str
+    user: str
+
+
+query, response_writer = pw.io.http.rest_connector(
+    host=HTTP_HOST,
+    port=int(HTTP_PORT),
+    schema=QueryInputSchema,
+    autocommit_duration_ms=50,
+)
+
+model = OpenAIChat(
+    api_key=API_KEY,
+    model=MODEL_LOCATOR,
+    temperature=TEMPERATURE,
+    max_tokens=MAX_TOKENS,
+    retry_strategy=pw.udfs.FixedDelayRetryStrategy(),
+    cache_strategy=pw.udfs.DefaultCache(),
+)
+
+response = query.select(
+    query_id=pw.this.id, result=model(prompt_chat_single_qa(pw.this.query))
+)
+
+response_writer(response)
+pw.run()
